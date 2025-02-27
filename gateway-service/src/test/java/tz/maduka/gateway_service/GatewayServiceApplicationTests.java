e3d70810e02cbff28467caebc2b0a36a057b7081
frank <frank.kessi@eganet.go.tz>
2025-02-27 19:56:27 +0300

    Configured api-gateway and registered to service-registry

---
+++ gateway-service/src/test/java/tz/maduka/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tz.maduka.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
